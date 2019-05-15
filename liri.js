dff2edd84db01c59f3554e06efdb4be6828e529b
Lilu <KHussell@gmail.com>
2019-05-15 13:35:16 -0400

    added movieThis to the commands object of functions

--- liri.js
+++ liri.js
@@ -13,19 +13,22 @@ var moment = require("moment")
 var spotify = new Spotify(keys.spotify)
 
 var input1 = process.argv[2]
-var input2 = "All the Small Things"
+var input2 = "Frozen"
 
-//function that will run from switch of user's first input and will take user's second input as parameter
-//2nd input will be put in bandsintown api queryURL and axios will retrieve data
-//console logging info about where band is playing
+//object of functions that will run from switch via user's first input
+//these functions will take user's second input as parameter
 var commands = {
+
     concertThis: function (input) {
+        //2nd input will be put in bandstown api queryURL and axios will retrieve data
         var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
         axios.get(queryURL).then(function (response) {
+            //loop through data and console log info about where band is playing
             for (var i = 0; i < response.data.length; i++) {
+                //using moment to format the datetime
                 var date = moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY")
 
-                console.log(`Venue: ${response.data[i].venue.name} \nLocation: ${response.data[i].venue.city}, ${response.data[i].venue.country} \nDate: ${date} \n ----------------------------------------`)
+                console.log(`----------------------------------------\nVenue: ${response.data[i].venue.name} \nLocation: ${response.data[i].venue.city}, ${response.data[i].venue.country} \nDate: ${date} \n ----------------------------------------`)
             }
         })
 
@@ -37,26 +40,38 @@ var commands = {
             input = "The Sign"
         }
         spotify.search({ type: 'track', query: `${input}`, limit: 20 }, function (err, data) {
+            //if err exists console log error
             if (err) {
                 return console.log("Error: " + err)
             }
-            
+           //loop through each search result and console log needed data 
             for (var i = 0; i < data.tracks.items.length; i++) {
-
-                console.log(`Artist(s): ${data.tracks.items[i].artists[0].name} \nSong: ${data.tracks.items[i].name}\nPreview link: ${data.tracks.items[i].album.external_urls.spotify}\nAlbum: ${data.tracks.items[i].album.name}\n----------------------------------------`)
+                console.log(`----------------------------------------\nArtist(s): ${data.tracks.items[i].artists[0].name} \nSong: ${data.tracks.items[i].name}\nPreview link: ${data.tracks.items[i].album.external_urls.spotify}\nAlbum: ${data.tracks.items[i].album.name}\n----------------------------------------`)
             }
         })
 
     },
 
     movieThis: function(input){
-
+      //if input has no movie, we will default it to Mr. Nobody
+      if(input=== undefined){
+          input="Mr. Nobody"
+      }
+      //url w/ trilogy api and added input to access data
+      let queryURL= `http://www.omdbapi.com/?apikey=trilogy&t=${input}`
+      //using axios to retrieve omdb api data
+      axios.get(queryURL).then(function(response){
+        //formatting date to just year of release  
+        let date= moment(response.data.Year, "DD MMM YYYY").format("YYYY")
+        //console logging all needed info from api data response
+        console.log(`----------------------------------------\nTitle: ${response.data.Title} \nYear: ${date}\nIMDB Rating: ${response.data.imdbRating}\nRotten Tomatoes: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}\n----------------------------------------`)
+      })
     }
 
 
 }
 
-commands.spotifyThisSong(input2)
+commands.movieThis(input2)
 
 var userCommand = function (input) {
     switch (input) {
